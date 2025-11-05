1637440397d97c51ad688fdcc8e2c96c6db48d77
Vlad Korsakov <ulqba@student.kit.edu>
2025-11-05 09:25:22 +0100

    update __init__.py

---
+++ kent_distribution/__init__.py
@@ -0,0 +1,19 @@
+from .kent_distribution import (
+	KentDistribution,
+	kent,
+	kent2,
+	kent3,
+	kent4,
+	kent_me,
+	kent_mle,
+)
+
+__all__ = [
+	"KentDistribution",
+	"kent",
+	"kent2",
+	"kent3",
+	"kent4",
+	"kent_me",
+	"kent_mle",
+]
